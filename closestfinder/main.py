dcd7d046d8f094d4b58aad8e77e32635dffcba3e
ilkka <ilkka.o.pekkala@gmail.com>
2021-08-24 14:12:25 +0300

    Closestfinder ready for deployment

--- closestfinder/main.py
+++ closestfinder/main.py
@@ -2,19 +2,25 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 
+#Example function json payload required:
+#{"longitude":"-85.48", "latitude": "42.95"}
 def findclosest(request):
     request_json = request.get_json(silent=True)
-    approxlongitude = request_json.get("longitude")
-    approxlatitude = request_json.get("latitude")
-    
+    approxlongitude = float(request_json.get("longitude"))
+    approxlatitude = float(request_json.get("latitude"))
+    data = ""
+    margin = 0.05
+
     while True:
-        margin = 0.05
         query = f"SELECT name FROM `week9-2-323806.Breweries.names` WHERE (longitude BETWEEN ({approxlongitude}-{margin}) AND ({approxlongitude}+{margin})) AND (latitude BETWEEN ({approxlatitude}-{margin}) AND ({approxlatitude}+{margin})) LIMIT 1"
         query_job = client.query(query)  # Make an API request.
-        if len(query_job) > 0 or margin > 5:
-            break
-        else:
+        for x in query_job:
+            data = x[0]
+        if data == "":
             margin *= 2
-    if margin > 5:
-        return "Sorry, there is no pub close to those coordinates"
-    return query_job[0]
+        else:
+            break
+        if margin > 5:
+            return "Sorry, there is no pub close to those coordinates"
+        
+    return data
